b205fe617affe76c003dac7862ced2fe2cbf4b0b
Eliezer Perl <eliezerrules@gmail.com>
2024-11-18 17:16:04 +0200

    vercel deploy

--- server/auth-server/main.py
+++ server/auth-server/main.py
@@ -16,5 +16,5 @@ app.add_middleware(
 
 app.include_router(router)
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", port=5000, reload=True, access_log=False)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", port=5000, reload=True, access_log=False)
